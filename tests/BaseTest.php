03630355fb6175756d45aeebadfe857e20fe2ad1
owen <owen@selesti.com>
2018-09-14 14:01:54 +0100

    wip

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -1,6 +1,18 @@
 <?php
 
+require_once __DIR__ . '/../vendor/illuminate/support/helpers.php';
+
+if (!function_exists('dd')) {
+    function dd()
+    {
+        $args = func_get_args();
+        call_user_func_array('dump', $args);
+        die();
+    }
+}
+
 abstract class BaseTest extends \PHPUnit\Framework\TestCase
 {
 
 }
+
--- tests/TikaTranslateTest.php
+++ tests/TikaTranslateTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use Selesti\TikaTranslate;
 use Selesti\TikaTranslate\TikaService;
 
 class TikaTranslateTest extends BaseTest
@@ -8,12 +7,27 @@ class TikaTranslateTest extends BaseTest
     public function test_i_can_connect_to_tika()
     {
         // arrange
-        $this->tika = new TikaService;
+        $tika = new TikaService;
 
         // action
+        $client = $tika->client();
 
         // assert
+        $this->assertInstanceOf(Vaites\ApacheTika\Clients\WebClient::class, $client);
+    }
 
+    public function test_i_can_extract_a_text_file()
+    {
+        // arrange
+        $tika = new TikaService;
+
+        // action
+        $text = $tika->text(
+            __DIR__ . '/../examples/text-file.txt'
+        );
+
+        // assert
+        $this->assertEquals('bonjour', $text);
     }
 
 }
